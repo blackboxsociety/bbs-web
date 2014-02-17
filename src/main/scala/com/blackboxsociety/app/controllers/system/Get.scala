e3abaf3838b6de4766df46547db56e18455e91c1
agro <agro@jantox.com>
2014-02-16 23:42:14 -0600

    Added LESS support with less-sbt.

--- src/main/scala/com/blackboxsociety/app/controllers/system/Get.scala
+++ src/main/scala/com/blackboxsociety/app/controllers/system/Get.scala
@@ -7,13 +7,22 @@ import scalaz.concurrent._
 import scalaz.concurrent.Future._
 import com.blackboxsociety.http.responses._
 import com.blackboxsociety.app.services._
+import java.io.File
 
 case class Get(services: ServiceManifest) extends Controller {
 
   val route = HttpRoute(MethodRoute(HttpGet), PathRoute("/"))
 
   def action(request: HttpRequest): Future[HttpResponse] = now {
-    Ok(":-/ this isn't the droid you were looking for.")
+    if(request.resource.path.startsWith("/assets/")) {
+      val file = request.resource.path.substring(8)
+      if(new File("target/scala-2.10/public/" + file).exists())
+        Ok(scala.io.Source.fromFile("target/scala-2.10/public/" + file).mkString)
+      else
+        Ok("/assets/" + file + " doesn't exist.")
+    } else {
+      Ok(":-/ this isn't the droid you were looking for.")
+    }
   }
 
 }
--- src/main/scala/com/blackboxsociety/app/views/Layout.scala
+++ src/main/scala/com/blackboxsociety/app/views/Layout.scala
@@ -11,14 +11,17 @@ trait Layout extends ScalaView {
       Tags2.title("Black Box Society"),
       meta(charset := "utf-8"),
       meta(name := "viewport", content := "width=device-width, initial-scale=1.0"),
-      meta(name := "description", content := "Spreading and connecting information")
+      meta(name := "description", content := "Spreading and connecting information"),
+      link(rel := "stylesheet", `type` := "text/css", href := "/assets/css/main.css")
     ),
     body(
       div(`class`:="top nav")(
         div(`class`:="brand pull-left")(
           a(href := "/")("Black Box Society")
         ),
-        div(`class`:="identity-box pull-right")
+        div(`class`:="identity-box pull-right blue")(
+          "Hello world!"
+        )
       ),
       container
     )
