f36a406efcc5c9d20b686dffaa6e3baf107570f5
agro <agro@jantox.com>
2014-02-18 16:19:24 -0600

    fixed conflicts

--- src/main/scala/com/blackboxsociety/app/views/Layout.scala
+++ src/main/scala/com/blackboxsociety/app/views/Layout.scala
@@ -28,34 +28,6 @@ trait Layout extends HtmlView {
     </html>
 
   def container: Elem
-=======
-import scalatags.Tags2.title
-import scalatags.HtmlTag
-import scalatags.all._
-
-trait Layout extends ScalaView {
-
-  def render: HtmlTag = html(
-    head(
-      title("Black Box Society"),
-      meta(charset := "utf-8"),
-      meta(name := "viewport", content := "width=device-width, initial-scale=1.0"),
-      meta(name := "description", content := "Spreading and connecting information"),
-      link(rel := "stylesheet", `type` := "text/css", href := "/assets/css/main.css")
-    ),
-    body(
-      div(`class`:="top nav")(
-        div(`class`:="brand pull-left")(
-          a(href := "/")("Black Box Society")
-        ),
-        div(`class`:="identity-box pull-right")
-      ),
-      container
-    )
-  )
-
-  def container: HtmlTag
->>>>>>> upstream/master
 
   override def toString = s"<!DOCTYPE html>\n${render.toString()}"
 
