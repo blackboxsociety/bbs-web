5c6495283b1a075a076006138a0c47d60e117df7
agro <agro@jantox.com>
2014-04-24 16:46:55 -0500

    Fixes public file serving. Closes #41.

--- src/main/scala/com/blackboxsociety/app/controllers/user/Get.scala
+++ src/main/scala/com/blackboxsociety/app/controllers/user/Get.scala
@@ -5,10 +5,7 @@ import com.blackboxsociety.http.routes._
 import com.blackboxsociety.http._
 import scalaz.concurrent._
 import scalaz.concurrent.Task._
-import com.blackboxsociety.http.responses._
 import com.blackboxsociety.app.services._
-import com.blackboxsociety.app.views._
-import scala.util.matching.Regex
 
 case class Get(implicit services: ServiceManifest) extends Controller {
 
