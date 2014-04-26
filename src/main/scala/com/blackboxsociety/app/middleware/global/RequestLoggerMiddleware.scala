ca9728e41c5d51ac476492b65e2b0fcbc71ed7c3
Joseph Moniz <joseph.moniz@gmail.com>
2014-04-25 20:14:57 -0700

    Added global middleware support. Closes #40
    Added global middleware via the front object. Uses the same semantics
    as controller specific middleware. I also implemented an overly simple
    request logger to demonstrate how to use it.

---
+++ src/main/scala/com/blackboxsociety/app/middleware/global/RequestLoggerMiddleware.scala
@@ -0,0 +1,15 @@
+package com.blackboxsociety.app.middleware.global
+
+import scalaz.concurrent.Task
+import com.blackboxsociety.http._
+
+object RequestLoggerMiddleware {
+
+  def apply(next: HttpRequest => Task[HttpResponse]): HttpRequest => Task[HttpResponse] = { (request) =>
+    next(request) map { response =>
+      println(s"${request.method} ${request.resource.path} -> ${response.statusCode}")
+      response
+    }
+  }
+
+}
