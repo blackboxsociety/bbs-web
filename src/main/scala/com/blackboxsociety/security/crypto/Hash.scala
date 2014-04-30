e85d5552044876283a30ee2d350593622daf0f82
Joseph Moniz <joseph.moniz@gmail.com>
2014-04-29 20:00:03 -0700

    Etag Caching Middleware
    Created a middleware that does correct etag based caching so we can avoid
    transmitting data the client already has.

--- src/main/scala/com/blackboxsociety/security/crypto/Hash.scala
+++ src/main/scala/com/blackboxsociety/security/crypto/Hash.scala
@@ -2,6 +2,7 @@ package com.blackboxsociety.security.crypto
 
 import javax.crypto.spec.SecretKeySpec
 import javax.crypto.Mac
+import java.security.MessageDigest
 
 object Hash {
 
@@ -11,6 +12,10 @@ object Hash {
     val mac  = Mac.getInstance(algo)
     mac.init(key)
     mac.doFinal(in.getBytes).map("%02X" format _).mkString
+  }
+
+  def sha1(bytes: Array[Byte]): String = {
+    MessageDigest.getInstance("SHA-1").digest(bytes).map("%02X" format _).mkString
   }
 
 }
